0431ab511c9423ae1f05d33719bb214309866702
Coby <cobyrpilpel2@gmail.com>
2020-12-16 00:20:23 -0500

    commit tree 549f0cf24b350b4e2542b06e9d35f0170deeeb47

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+# Add  code here!      
+def prime?(num)
+    if num <= 1
+        return false
+    end
+    i=2
+        while i <= num/2
+            if num % i == 0
+return false
+            else
+                i += 1
+        end
+end
+    true
+end 
+
+#did some googling on this one. Do more research. Meme = true.
+
+
